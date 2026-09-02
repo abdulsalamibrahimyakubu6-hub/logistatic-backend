e0ef5032d93a45638e8e3ef1937f70630e0f577c
abdulsalamibrahimyakubu6-hub <abdulsalamibrahimyakubu6@gmail.com>
2026-09-02 22:19:56 +0100

    Build in Bash

--- build.sh
+++ build.sh
@@ -7,3 +7,20 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
 python manage.py migrate
+
+
+# Safely create superuser
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+import os
+User = get_user_model()
+username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print("Superuser created.")
+    else:
+        print("Superuser already exists.")
+EOF
