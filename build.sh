3243ca9311b5e5bc5cb98397a0657408a8803f2c
abdulsalamibrahimyakubu6-hub <abdulsalamibrahimyakubu6@gmail.com>
2026-09-11 15:49:29 +0100

    changing password

--- build.sh
+++ build.sh
@@ -9,18 +9,20 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 
-# Safely create superuser
+# Safely change user password
 python manage.py shell << EOF
 from django.contrib.auth import get_user_model
 import os
+
 User = get_user_model()
-username = os.getenv("admin")
-email = os.getenv("admin@gmail.com")
-password = os.getenv("admin123abc")
-if username and email and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email, password=password)
-        print("Superuser created.")
-    else:
-        print("Superuser already exists.")
+username = os.getenv("DJANGO_ADMIN_USERNAME", "admin")
+new_password = os.getenv("DJANGO_ADMIN_PASSWORD", "admin123abc")
+
+try:
+    user = User.objects.get(username=username)
+    user.set_password(new_password)
+    user.save()
+    print(f"Password updated successfully for user: {username}")
+except User.DoesNotExist:
+    print(f"User '{username}' does not exist. No password was changed.")
 EOF
